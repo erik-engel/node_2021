09cd98223f7d82184df6d3c84e4e9b8add0eca39
erik <erik1500@stud.kea.dk>
2021-02-19 10:12:47 +0100

    commit for updating

--- First_Server/app.js
+++ First_Server/app.js
@@ -1,16 +1,43 @@
-const express = require("express");
+// Import package
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
+// Execute package
 const app = express();
 
-// to solve the assignment create a datatype to store the data in
-// implement the two GET endpoints .. you can hardcode the id for now
 
-app.get("/", (req, res) => {
-    res.send({ message: "This is my response" });
+// Middlewares
+// Functions that'll alwayes execute, when we hit a specific route
+// We can f.eks use some kind of authetication like 
+// app.use(auth);
+
+
+
+// Routes 
+app.get('/', (req,res) => {
+    res.send('We are on home');
 });
 
-app.get("/newOne/id", (req, res) => {
-    const id = 1;
-    res.send({ "something": "That you can do" });
+app.get('/posts', (req,res) => {
+    res.send('We are on posts');
 });
 
+app.get('/welcome', (req, res) => {
+    res.send('<h1>Welcome<h1>')
+})
+
+app.get('/me', (req, res) => {
+    res.send({name : 'Erik', age : 25, course : 'node.js'})
+})
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useUnifiedTopology: true }, 
+    { useNewUrlParser: true },
+    () => console.log('connected to DB!')
+);
+
+
+// How do we start listening to the server?
 app.listen(8080);
